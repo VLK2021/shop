16d0f472fa7b16a4892bfbb1e57bac7784e9ff65
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-21 23:19:52 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app.routing.module";
+import {MainLayoutComponent} from "./app-components/main-layout/main-layout.component";
+import {MainPageComponent} from "./app-components/main-page/main-page.component";
+import {ProductPageComponent} from "./app-components/product-page/product-page.component";
+import {CartPageComponent} from "./app-components/cart-page/cart-page.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {MainLayoutComponent} from "./app-components/main-layout/main-layout.component";
+import {MainPageComponent} from "./app-components/main-page/main-page.component";
+import {ProductPageComponent} from "./app-components/product-page/product-page.component";
+import {CartPageComponent} from "./app-components/cart-page/cart-page.component";
+
+
+
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+      {path: '', component: MainPageComponent},
+      {path: 'product/:id', component: ProductPageComponent},
+      {path: 'cart', component: CartPageComponent}
+    ]}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
