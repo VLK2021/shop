9574b2aee1b6caf1e82cfff8bbeb8abe73e67542
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-21 23:30:11 +0200

    routing

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
+
 import {MainLayoutComponent} from "./app-components/main-layout/main-layout.component";
 import {MainPageComponent} from "./app-components/main-page/main-page.component";
 import {ProductPageComponent} from "./app-components/product-page/product-page.component";
 import {CartPageComponent} from "./app-components/cart-page/cart-page.component";
 
 
-
 const routes: Routes = [
   {path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
       {path: '', component: MainPageComponent},
       {path: 'product/:id', component: ProductPageComponent},
       {path: 'cart', component: CartPageComponent}
