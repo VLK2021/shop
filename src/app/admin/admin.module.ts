5f1705ba5cbd051b780c37356a10b9bce72fd271
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-23 00:37:13 +0200

    admin routing finished

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,22 +1,40 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule} from "@angular/router";
 
-import { AdminLayoutComponent } from './admin-components/admin-layout/admin-layout.component';
+import {AdminLayoutComponent} from './admin-components/admin-layout/admin-layout.component';
+import {LoginPageComponent} from "./admin-components/login-page/login-page.component";
+import { AddPageComponent } from './admin-components/add-page/add-page.component';
+import { DashboardPageComponent } from './admin-components/dashboard-page/dashboard-page.component';
+import { OrdersPageComponent } from './admin-components/orders-page/orders-page.component';
+import {EditPageComponent} from "./admin-components/edit-page/edit-page.component";
 
 
 @NgModule({
   declarations: [
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    AddPageComponent,
+    DashboardPageComponent,
+    OrdersPageComponent,
+    EditPageComponent,
+    LoginPageComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path: '', component: AdminLayoutComponent, children:[
-
-        ]}
+      {
+        path: '', component: AdminLayoutComponent, children: [
+          {path:'', redirectTo: '/admin/login', pathMatch: 'full'},
+          {path: 'login', component: LoginPageComponent},
+          {path: 'dashboard', component: DashboardPageComponent},
+          {path: 'orders', component: OrdersPageComponent},
+          {path: 'add', component: AddPageComponent},
+          {path: 'product/:id/edit', component: EditPageComponent},
+        ]
+      }
     ])
   ],
-  exports: []
+  exports: [RouterModule]
 })
-export class AdminModule { }
+export class AdminModule {
+}
