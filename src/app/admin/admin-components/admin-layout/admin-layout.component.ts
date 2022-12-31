78325728aedf4c5af6bc8e9408e0d5d0b21fb044
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-31 12:17:02 +0200

    board

--- src/app/admin/admin-components/admin-layout/admin-layout.component.ts
+++ src/app/admin/admin-components/admin-layout/admin-layout.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../../../app-components/auth.service";
 import {Router} from "@angular/router";
+
+import {AuthService} from "../../../app-components/auth.service";
+
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.css']
 })
+
 export class AdminLayoutComponent implements OnInit {
 
   constructor(public auth:AuthService,
-              private router:Router) { }
+              private router:Router) {
+  }
 
   ngOnInit(): void {
   }
 
   logout($event: MouseEvent) {
-    // @ts-ignore
-    event.preventDefault()
+    $event.preventDefault()
     this.auth.logout()
     this.router.navigate(['/admin', 'login'])
   }
