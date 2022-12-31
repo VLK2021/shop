cfbfcab03ad8e04c48a7e949dbad086eb0c5ddeb
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-31 19:15:05 +0200

    add products

--- src/app/admin/admin-components/add-page/add-page.component.ts
+++ src/app/admin/admin-components/add-page/add-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
   styleUrls: ['./add-page.component.css']
 })
-export class AddPageComponent implements OnInit {
 
-  constructor() { }
+export class AddPageComponent implements OnInit {
+  form: FormGroup;
+
+  constructor() {
+    this.createForm();
+  }
 
   ngOnInit(): void {
   }
 
+  createForm(): void {
+    this.form = new FormGroup<any>({
+      type: new FormControl(null, Validators.required),
+      tittle: new FormControl(null, Validators.required),
+      photo: new FormControl(null, Validators.required),
+      info: new FormControl(null, Validators.required),
+      price: new FormControl(null, Validators.required),
+    })
+  };
+
+  submit() {
+
+  };
 }
