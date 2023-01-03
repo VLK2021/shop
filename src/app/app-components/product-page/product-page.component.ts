f259d2edd3ae2cf3607e30434714d0dd20d10904
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-01-03 23:07:04 +0200

    did get method and render products

--- src/app/app-components/product-page/product-page.component.ts
+++ src/app/app-components/product-page/product-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
+
 
   constructor() { }
 
