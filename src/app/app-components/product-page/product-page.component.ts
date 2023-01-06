7aefe173cbdcf5eb39e12d839728dda3f8032f97
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-01-06 19:48:51 +0200

    product-page doing

--- src/app/app-components/product-page/product-page.component.ts
+++ src/app/app-components/product-page/product-page.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
 import {ProductService} from "../../product.service";
-import {IProduct} from "../../interfaces/IProduct";
 
 
 @Component({
@@ -13,14 +12,14 @@ import {IProduct} from "../../interfaces/IProduct";
 
 export class ProductPageComponent implements OnInit {
 
-  product: IProduct;
+  product: any;
 
   constructor(private productService:ProductService,
-              private route:ActivatedRoute) { }
+              private activatedRoute:ActivatedRoute) { }
 
 
   ngOnInit(): void {
-    this.route.params.subscribe(({id}) => {
+    this.activatedRoute.params.subscribe(({id}) => {
       this.productService.getById(id).subscribe(value => this.product = value)
     })
   }
